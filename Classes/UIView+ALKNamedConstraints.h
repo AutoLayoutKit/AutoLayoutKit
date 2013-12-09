afa588e0d65df106b9362f70aab3db4771c825f5
Florian Krueger <florian.krueger@projectserver.org>
2013-12-09 23:50:42 +0100

    ALK #3 documentation of UIView+ALKNamedConstraints formatted and completed

--- Classes/UIView+ALKNamedConstraints.h
+++ Classes/UIView+ALKNamedConstraints.h
@@ -24,48 +24,75 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ The `ALKNamedConstraints` category adds a constraint store to every `UIView`
+ subclass. This store can be used to remember constraints by a given name. It is
+ possible to add, retrieve and remove constraints by a name that must be given
+ when added.
+ 
+ @since 0.1.0
+ */
 @interface UIView (ALKNamedConstraints)
 
+/** 
+ stores all the named constraints. 
+ 
+ @since 0.1.0
+ */
 @property (nonatomic, retain, setter = alk_setNamedConstraints:) NSMutableDictionary* alk_namedConstraints;
 
-/** Tries to add a constraint to the `UIView` while remembering the `constraint_name` for future
- *  reference.
- *
- *  @param name The identifier that the constraint should be identified with.
- *
- *  @return The added constraint or `nil` if the name is already used.
+/** 
+ Tries to add a constraint to the `UIView` while remembering the 
+ `constraint_name` for future reference.
+ 
+ @param constraint The `NSLayoutConstraint` to be referred to with `name`.
+ @param name The identifier that the constraint should be identified with.
+ 
+ @return The added constraint or `nil` if the name is already used.
+ 
+ @since 0.1.0
  */
-- (NSLayoutConstraint *)alk_addConstraint:(NSLayoutConstraint *)constraint withName:(NSString *)name;
+- (NSLayoutConstraint *)alk_addConstraint:(NSLayoutConstraint *)constraint
+                                 withName:(NSString *)name;
 
-/** Looks in the named constraints for a constraint named `name` and returns it when found, `nil`
- *  otherwise.
- *  
- *  @param name The identifier that was used during creation of the constraint.
- *
- *  @return The constrainted with the identifier `name` or `nil` if the name is not used for a 
- *  constraint.
+/** 
+ Looks in the named constraints for a constraint named `name` and returns it
+ when found, `nil` otherwise.
+ 
+ @param name The identifier that was used during creation of the constraint.
+ 
+ @return The constrainted with the identifier `name` or `nil` if the name is 
+ not used for a constraint.
+ 
+ @since 0.1.0
  */
 - (NSLayoutConstraint *)alk_constraintWithName:(NSString *)name;
 
-/** Takes an array of names and tries to remove all `NSLayoutConstraint` instances from the receiver
- *  which are referenced by the given names. If a name does not refer to a `NSLayoutConstraint`, it
- *  is simply ignored.
- *
- *  @param names The names of `NSLayoutConstraint` instances that have been added to the receiver 
- *  before, using {#addConstraint}:`withName:`.
- *
- *  @see removeConstraintWithName:
+/** 
+ Takes an array of names and tries to remove all `NSLayoutConstraint` instances 
+ from the receiver which are referenced by the given names. If a name does not 
+ refer to a `NSLayoutConstraint`, it is simply ignored.
+ 
+ @param names The names of `NSLayoutConstraint` instances that have been added 
+ to the receiver before, using {#addConstraint}:`withName:`.
+
+ @see -alk_removeConstraintWithName:
+ 
+ @since 0.1.0
  */
 - (void)alk_removeConstraintsWithNames:(NSArray *)names;
 
-/** Takes as single name and tries to remove the `NSLayoutConstraint` instance from the receiver
- *  which is referenced by the given name. If the name does not refer to a `NSLayoutConstraint`, it
- *  is simply ignored.
- *
- *  @param name The name of `NSLayoutConstraint` instances that have been added to the receiver
- *  before, using `addConstraint:withName:`.
- *
- *  @see removeConstraintsWithNames:
+/** 
+ Takes as single name and tries to remove the `NSLayoutConstraint` instance from 
+ the receiver which is referenced by the given name. If the name does not refer 
+ to a `NSLayoutConstraint`, it is simply ignored.
+
+ @param name The name of `NSLayoutConstraint` instances that have been added to 
+ the receiver before, using `addConstraint:withName:`.
+
+ @see -alk_removeConstraintsWithNames:
+ 
+ @since 0.1.0
  */
 - (void)alk_removeConstraintWithName:(NSString *)name;
 
