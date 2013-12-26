c5b42ffc53cd00e07c127d107a4f5b41fdee52ea
Florian Krueger <florian.krueger@projectserver.org>
2013-12-26 13:02:56 +0100

    Example Project: Prepared TextField Example

---
+++ Example/LayoutKitPrototype/LKPTextFieldView.m
@@ -0,0 +1,69 @@
+//  LKPTextFieldView.m
+//  AutoLayoutKitPrototype
+//
+//  Copyright (c) 2013 Florian Krueger <florian.krueger@projectserver.org>
+//  Created on 12/26/13.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#import "LKPTextFieldView.h"
+
+@interface LKPTextFieldView ()
+
+@end
+
+@implementation LKPTextFieldView
+
+#pragma mark - Memory Management
+
+- (void)dealloc
+{
+    
+}
+
+#pragma mark - Setup & Init
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    [self setupSubviews];
+    [self setupLayout];
+}
+
+#pragma mark - Setup & Init (Private)
+
+- (void)setupSubviews
+{
+    
+}
+
+- (void)setupLayout
+{
+    
+}
+
+@end
---
+++ Example/LayoutKitPrototype/LKPTextFieldViewController.m
@@ -0,0 +1,89 @@
+//  LKPTextFieldViewController.m
+//  AutoLayoutKitPrototype
+//
+//  Copyright (c) 2013 Florian Krueger <florian.krueger@projectserver.org>
+//  Created on 12/26/13.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#import "LKPTextFieldViewController.h"
+
+@interface LKPTextFieldViewController ()
+
+@end
+
+@implementation LKPTextFieldViewController
+
+#pragma mark - Memory Management
+
+- (void)dealloc
+{
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Setup & Init
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+- (void)setup
+{
+    self.title = @"TextField Example";
+}
+
+#pragma mark - View Lifecycle
+
+- (void)loadView
+{
+    LKPSimpleView *view = [[LKPSimpleView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    self.view = view;
+}
+
+@end
