2f103e21413da0c5a3d02966270da940431913c7
Florian Krueger <florian.krueger@projectserver.org>
2014-01-12 17:28:28 +0100

    ALK #6 recreated all tests from the rubymotion prototype

--- Example/AutoLayoutKitTests/AutoLayoutKitTests.m
+++ Example/AutoLayoutKitTests/AutoLayoutKitTests.m
@@ -9,6 +9,8 @@
 #import <XCTest/XCTest.h>
 #import "AutoLayoutKit.h"
 
+NSString * const kALKTestConstraint = @"ALKTestConstraint";
+
 @interface AutoLayoutKitTests : XCTestCase
 
 @end
@@ -27,15 +29,143 @@
     [super tearDown];
 }
 
+#pragma mark - Basic Tests
+
 - (void)testStopsTranslatingAutoresizingMasks
 {
   UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
   
-  XCTAssertTrue(view.translatesAutoresizingMaskIntoConstraints, @"Translation is already off before applying ALK");
+  XCTAssertTrue(view.translatesAutoresizingMaskIntoConstraints, @"");
   
   [ALKConstraints layout:view do:^(ALKConstraints *c) {}];
   
-  XCTAssertFalse(view.translatesAutoresizingMaskIntoConstraints, @"Translation isn't off after applying ALK");
+  XCTAssertFalse(view.translatesAutoresizingMaskIntoConstraints, @"");
 }
+
+#pragma mark - Target View Tests
+
+- (void)testCreatesUnrelatedConstraintOnTheGivenItemWhenUsingSet
+{
+  UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+  
+  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+    [c set:ALKWidth to:100.f];
+  }];
+  
+  NSArray *constraints = [view constraints];
+  XCTAssertEqual([constraints count], (NSUInteger)1, @"");
+}
+
+- (void)testCreatedRelatedConstraintOnTheSpecifiedItemWhenUsingMakeOn
+{
+  UIView *parentView  = [[UIView alloc] initWithFrame:CGRectZero];
+  UIView *childView   = [[UIView alloc] initWithFrame:CGRectZero];
+  [parentView addSubview:childView];
+  
+  [ALKConstraints layout:childView do:^(ALKConstraints *c) {
+    [c make:ALKWidth equalTo:parentView s:ALKWidth on:parentView];
+  }];
+  
+  XCTAssertEqual([childView.constraints count],   (NSUInteger)0, @"");
+  XCTAssertEqual([parentView.constraints count],  (NSUInteger)1, @"");
+}
+
+- (void)testCreatedRelatedConstraintOnTheParentItemWhenUsingMake
+{
+  UIView *parentView  = [[UIView alloc] initWithFrame:CGRectZero];
+  UIView *childView   = [[UIView alloc] initWithFrame:CGRectZero];
+  [parentView addSubview:childView];
+  
+  [ALKConstraints layout:childView do:^(ALKConstraints *c) {
+    [c make:ALKWidth equalTo:parentView s:ALKWidth];
+  }];
+  
+  XCTAssertEqual([childView.constraints count],   (NSUInteger)0, @"");
+  XCTAssertEqual([parentView.constraints count],  (NSUInteger)1, @"");
+}
+
+- (void)testCreatedRelatedConstraintOnTheCommonParentItemWhenUsingMake
+{
+  UIView *parentView = [[UIView alloc] initWithFrame:CGRectZero];
+  UIView *childView1 = [[UIView alloc] initWithFrame:CGRectZero];
+  UIView *childView2 = [[UIView alloc] initWithFrame:CGRectZero];
+  [parentView addSubview:childView1];
+  [parentView addSubview:childView2];
+  
+  [ALKConstraints layout:childView1 do:^(ALKConstraints *c) {
+    [c make:ALKWidth equalTo:childView2 s:ALKWidth];
+  }];
+  
+  XCTAssertEqual([childView1.constraints count], (NSUInteger)0, @"");
+  XCTAssertEqual([childView2.constraints count], (NSUInteger)0, @"");
+  XCTAssertEqual([parentView.constraints count], (NSUInteger)1, @"");
+}
+
+#pragma mark - Named Constraints Tests
+
+- (void)testRetrieveAConstraintByItsName
+{
+  UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+  CGFloat constant = 111.f;
+  
+  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+    [c set:ALKWidth to:constant name:kALKTestConstraint];
+  }];
+  
+  NSLayoutConstraint *constraint = [view alk_constraintWithName:kALKTestConstraint];
+  
+  XCTAssertNotNil(constraint, @"");
+  XCTAssertEqualWithAccuracy(constraint.constant, constant, 0.001, @"");
+}
+
+- (void)testRetrieveNonExistingConstraintByName
+{
+  UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+  
+  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+    [c set:ALKWidth to:111.f];
+  }];
+  
+  XCTAssertNil([view alk_constraintWithName:kALKTestConstraint], @"");
+}
+
+- (void)testNotPossibleToAddTwoConstraintsWithTheSameName
+{
+  UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+  CGFloat constant1 = 111.f;
+  CGFloat constant2 = 222.f;
+  
+  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+    [c set:ALKWidth to:constant1 name:kALKTestConstraint];
+  }];
+  
+  NSLayoutConstraint *constraint = [view alk_constraintWithName:kALKTestConstraint];
+  
+  XCTAssertNotNil(constraint, @"");
+  XCTAssertEqualWithAccuracy(constraint.constant, constant1, 0.001, @"");
+  
+  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+    [c set:ALKHeight to:constant2 name:kALKTestConstraint];
+  }];
+  
+  constraint = [view alk_constraintWithName:kALKTestConstraint];
+  
+  XCTAssertNotNil(constraint, @"");
+  XCTAssertEqualWithAccuracy(constraint.constant, constant1, 0.001, @"");
+  XCTAssertNotEqualWithAccuracy(constraint.constant, constant2, 0.001, @"");
+}
+
+#pragma mark - Layout Tests (SET)
+
+//- (void)testLayout
+//{
+//  UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+//  
+//  [ALKConstraints layout:view do:^(ALKConstraints *c) {
+//    [c set:ALKWidth to:100.f];
+//  }];
+//  
+//  [view.constraints count]
+//}
 
 @end
